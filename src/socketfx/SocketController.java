8457252732a74f51a01e45c03b612e4fc8158362
Arturo Cordero <vakmancordero@gmail.com>
2017-02-02 19:44:33 -0600

    Inicializando repositorio

---
+++ src/socketfx/SocketController.java
@@ -0,0 +1,18 @@
+package socketfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Arturh
+ */
+public class SocketController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    } 
+    
+}
